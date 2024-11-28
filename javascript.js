366a684f6f6f1fd64e0a311864e0f3c4e30f62e8
aliciamorillo <alicia.morillo87@gmail.com>
2024-11-28 13:46:27 +0100

    proyecto calculadora

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function valorCampo(value) {
+    document.getElementById("result").value += value;
+}
+
+function calcular() {
+    const resultado = document.getElementById("result");
+    try {
+        resultado.value = eval(resultado.value);
+    } catch {
+        resultado.value = "Error";
+    }
+}
+
+function limpiarResultado() {
+    document.getElementById("result").value = "";
+}
